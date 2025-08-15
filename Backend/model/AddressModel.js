e7632e145125adb100a1312a5cb2eab9b49d97dd
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-08-15 20:58:10 +0530

    AddressModel added

---
+++ Backend/model/AddressModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const addressSchema=new mongoose.Schema({
+    address_line:{
+        type:String,
+        required:[true,"Address line is required"],
+        default:""
+    },
+    city:{
+        type:String,
+        default:""
+    },
+    state:{
+        type:String,
+        default:""
+    },
+    pincode:{
+        type:String, 
+        required:[true,"Pincode is required"],   
+    },
+    country:{
+        type:String
+    },
+    mobile:{
+        type:Number,
+        default:null
+    }
+},{timestamps:true});
+
+const AddressModel=mongoose.model("address", addressSchema);
+
+export default AddressModel;
